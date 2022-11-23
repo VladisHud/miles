ce3dd91798696d91712cf6a11026885e5f1890fb
VladisHud <savanchukvlad899@gmail.com>
2022-11-23 15:56:54 +0200

    miles1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount = 30;
+        int amount = 1000;
         int bonus = 20;
         int miles = (amount / bonus);
         System.out.println(miles);
